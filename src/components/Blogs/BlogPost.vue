eae13d09386d3247a52b623500cc2dc955be52fb
yuu <myduyen14125@gmail.com>
2021-07-28 19:15:29 +0700

    outline blog-post

---
+++ src/components/Blogs/BlogPost.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="blog-wrapper">
+    <div class="blog-content">
+      <div>
+        <h2 v-if="post.welcomeScreen">{{ post.title }}</h2>
+        <h2 v-else>{{ post.title }}</h2>
+        <p v-if="post.welcomeScreen">{{ post.blogPost }}</p>
+        <p class="content-preview" v-else>{{ post.blogHTML }}</p>
+        <router-link v-if="post.welcomeScreen" to="#">
+          Login/Register<Arrow class="arrow arrow-light" />
+        </router-link>
+        <router-link v-else to="#">
+          View The Post<Arrow class="arrow arrow-light" />
+        </router-link>
+      </div>
+      <div class="blog-photo">
+        <img v-if="post.welcomeScreen" :src="require(`../../assets/blogPhotos/${post.photo}.jpg`)" alt="">
+        <img v-else :src="require(`../../assets/blogPhotos/${post.blogCoverPhoto}.jpg`)" alt="">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import Arrow from "../../assets/Icons/arrow-right-light.svg"
+export default {
+  name: "blogPost",
+  props: ["post"],
+  // components: { Arrow },
+};
+</script>
+
+<style lang="scss" scoped>
+.blog-wrapper {
+  display: flex;
+  flex-direction: column;
+  margin-top: 55px;
+  @media (min-width: 768px) {
+    min-height: 650px;
+    max-height: 650px;
+    flex-direction: row;
+  }
+
+  .blog-content {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex: 4;
+    order: 2;
+    @media (min-width: 768px){
+      order: 1;
+      flex: 3;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,36 @@
 <template>
   <div class="home">
+    <BlogPost :post="welcomeScreen" />
+    <BlogPost :post="post" v-for="(post, index) in sampleBlogPost" :key="index" />
   </div>
 </template>
 
 <script>
+import BlogPost from "../components/Blogs/BlogPost"
 export default {
   name: "Home",
-  components: {},
+  components: {BlogPost},
+  data() {
+    return {
+      welcomeScreen: {
+        title: "WELCOME!",
+        blogPost: "Mình là hehe. Chào mừng bạn đến với blog của mình. Đây là nơi mình thường xuyên chia sẻ về những câu chuyện nho nhỏ trong đời sống thường ngày của mình. Mình tin rằng, hạnh phúc đến từ những điều giản dị nhất. Hi vọng bạn sẽ tìm thấy được sự bình yên và một chút niềm vui khi đọc blog của mình.",
+        welcomeScreen: true,
+        photo: "minimalism",
+      },
+      sampleBlogPost: [
+        {
+          title: "This is a filter Title!",
+          blogHTML: "This is blog post filter content",
+          blogCoverPhoto: "beautiful-stories",
+        },
+        {
+          title: "This is a filter Title!2",
+          blogHTML: "This is blog post filter content",
+          blogCoverPhoto: "designed-for-everyone",
+        }
+      ]
+    }
+  }
 };
 </script>
