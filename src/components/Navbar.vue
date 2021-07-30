3e4284efb17fff3dcd84a3d9d5329bdce116f3d0
Hoang Nhan <nhan053qt@gmail.com>
2021-07-30 16:41:23 +0700

    small fix on profile-menu animation

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -269,13 +269,15 @@ export default {
         box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
         border-radius: 5px;
         text-align: left;
+        overflow: hidden;
 
         .info {
           display: flex;
           align-items: center;
           padding: 15px;
           border-bottom: 1px solid white;
-
+          cursor: initial; 
+          
           .initials {
             position: initial;
             width: 40px;
