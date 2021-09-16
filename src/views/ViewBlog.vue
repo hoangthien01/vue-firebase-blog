979da9c1116837abb1db592ac0d37280fd56c343
nt-myduyen <myduyen14125@gmail.com>
2021-09-16 17:50:18 +0700

    edit toggle editing each blog

--- src/views/ViewBlog.vue
+++ src/views/ViewBlog.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="post-view" v-if="currentBlog">
+    <div class="toggle-edit">
+      <span>Toggle Editing Post</span>
+      <input type="checkbox" v-model="activeEditPost" :checked="activeEditPost" @click="editPost">
+    </div>
+    <div class="icons hehe" v-bind:class="{showIcons: activeEditPost}">
+      <div @click="editBlog" class="icon">
+        <Edit class="edit" />
+      </div>
+      <div @click="deletePost" class="icon">
+        <Delete class="delete" />
+      </div>
+    </div>
+
     <div class="container quillWrapper">
       <h2>{{ this.currentBlog[0].blogTitle }}</h2>
       <h4>Posted on: {{ new Date(this.currentBlog[0].blogDate).toLocaleString("en-us", { dateStyle: "long" }) }}</h4>
@@ -47,8 +60,12 @@
 <script>
 import db from '../firebase/firebaseInit'
 import firebase from 'firebase'
+import Edit from "../assets/Icons/edit-regular.svg";
+import Delete from "../assets/Icons/trash-regular.svg";
+
 export default {
   name: "ViewBlog",
+  components: { Edit, Delete },
   data() {
     return {
       currentBlog: null,
@@ -57,6 +74,7 @@ export default {
       comments: [],
       quantityMessages : 0,
       errorMsg: '',
+      activeEditPost: true
     }
   },
   async mounted() {
@@ -83,8 +101,30 @@ export default {
     profileId() {
       return this.$store.state.profileId;
     },
+    // editPost: { 
+    //   get() {
+    //     return this.$store.state.editPost;
+    //   },
+    //   set(payload) {
+    //     this.$store.commit("toggleEditPost", payload)
+    //   }
+    // },
+    // activeEditPost() {
+    //   return this.$store.state.editPost;
+    // }
   },
   methods : {
+    editPost() {
+      
+    },
+    deletePost() {
+      this.$store.dispatch("deletePost", this.currentBlog[0].blogID)
+      console.log("deletePost")
+    },
+    editBlog() {
+      this.$router.push({ name: "EditBlog", params: { blogID: this.currentBlog[0].blogID } });
+      console.log("editPost")
+    },
     async sendMessage() {
       if(this.comment == '') {
         this.errorMsg = "Comment not be empty"
@@ -170,6 +210,87 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.toggle-edit {
+  display: flex;
+  align-items: center;
+  position: absolute;
+  top: 70px;
+  right: 20px;
+
+  span {
+    margin-right: 16px;
+  }
+  //Toggle checkbox
+input[type="checkbox"] {
+  position: relative;
+  border: none;
+  -webkit-appearance: none;
+  background: #fff;
+  outline: none;
+  width: 55px;
+  height: 25px;
+  border-radius: 20px;
+  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+}
+input[type="checkbox"]:before {
+  content: "";
+  position: absolute;
+  width: 25px;
+  height: 25px;
+  border-radius: 20px;
+  top: 0;
+  right: 0;
+  background: #303030;
+  transform: scale(1.1);
+  transition: 750ms ease all;
+  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+}
+input:checked[type="checkbox"]:before {
+  background: #f3f3f3;
+  right: 32px;
+}
+}
+.showIcons {
+  display: none;
+}
+.icons {
+  position: absolute;
+  top: 100px;
+  right: 10px;
+  z-index: 10;
+
+  .icon {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 8px;
+    width: 35px;
+    height: 35px;
+    border: 1px solid #252525;
+    border-radius: 50%;
+    background-color: #fff;
+    transition: 0.5s;
+
+    &:hover {
+      background-color: #252525;
+
+      .edit, .delete {
+        path {
+          fill: #fff;
+        }
+      }
+    }
+    .edit, .delete {
+      pointer-events: none;
+      height: 15px;
+      width: auto;
+    }
+    &:nth-child(1) {
+      margin-right: 8px;
+    }
+  }
+}
+
 .post-view {
   h2{
     text-align: center;
